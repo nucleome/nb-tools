82499445eeef8626fc67cc9f41f46b4bc4afa292
zhuxp <nimezhu@gmail.com>
2020-03-17 14:24:51 -0400

    init npm

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export {default as getUrlParam} from "./getUrlParam"
+export {default as isInt} from "./isInt"
+export {default as isRgb} from "./isRgb"
+export {default as parseBed} from "./parseBed"
+export {default as parseInts} from "./parseInts"
+export {default as parseRegion} from "./parseRegion"
+export {default as parseRegions} from "./parseRegions"
+export {default as randomColor} from "./randomColor"
+export {default as randomString} from "./randomString"
+export {default as regionNiceText} from "./regionNiceText"
+export {default as regionsNiceText} from "./regionsNiceText"
+export {default as regionsSmartText} from "./regionsSmartText"
+
+export {default as regionText} from "./regionText"
+export {default as regionsText} from "./regionsText"
+
+export {default as strToColor} from "./strToColor"
+
+export {default as webglSupport} from "./webglSupport"
